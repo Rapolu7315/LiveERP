1f2598471bbd2cd76ee4895e018eecbdd7d8fa61
VENKY <VENKY@LAPTOP-COONM8IM>
2023-05-03 20:04:28 +0530

    First commit

--- src/test/java/common/FunctionsLibrary.java
+++ src/test/java/common/FunctionsLibrary.java
@@ -1,5 +1,128 @@
 package common;
 
-public class FunctionsLibrary {
+import java.io.FileInputStream;
 
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class FunctionsLibrary {
+	public static WebDriver driver;
+	public static Properties conpro;
+	public static String Expected;
+	public static String Actual;
+	
+	//method for launch browser
+	public static WebDriver StartBrowser() throws Throwable
+	{
+		conpro = new Properties();
+		conpro.load(new FileInputStream("D:\\eclipse\\workspace\\ERP_Hybrid\\PropertyFile"));
+		if(conpro.getProperty("Browser").equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+		
+		}
+		else if(conpro.getProperty("Browser").equalsIgnoreCase("firfox"))
+		{
+			driver = new FirefoxDriver();
+			driver.manage().deleteAllCookies();
+		}
+		else
+		{
+			System.out.println("browser value is not matching");
+		}
+		return driver;
+		
+		
+	}
+//method for launch Url
+	public static void openurl(WebDriver driver)
+	{
+		driver.get(conpro.getProperty("Url"));
+	}
+	
+//method for wait for element
+	public static void waitForElement(WebDriver driver,String LocatorType,String LocatorValue,String WaitTime)
+	{
+		WebDriverWait myWait = new WebDriverWait(driver, Integer.parseInt(WaitTime));
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.name(LocatorValue)));
+			
+		}
+		else if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+		else if(LocatorType.equalsIgnoreCase("id"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(LocatorValue)));
+		}
+		}
+	//method for Text boxes
+	
+	public static void typeAction(WebDriver driver,String LocatorType,String LocatorValue, String TextData)
+	{
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.id(LocatorValue)).clear();
+			driver.findElement(By.id(LocatorValue)).sendKeys(TextData);
+		}
+		else if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TextData);
+		}
+		else if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.name(LocatorValue)).clear();
+			driver.findElement(By.name(LocatorValue)).sendKeys(TextData);
+		}
+	}
+	//method for button,links,radiobuttons,checkboxs and images
+	
+	public static void clickAction(WebDriver driver, String LocatorType,String LocatorValue)
+	{
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.name(LocatorValue)).click();
+			
+		}
+		else if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.id(LocatorValue)).sendKeys(Keys.ENTER);
+		}
+		else if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+	}
+	
+	//method for validating title
+	public static void validateTitle(WebDriver driver,String Expected_Title)
+	{
+		String Actual_Title = driver.getTitle();
+		try
+		{
+		Assert.assertEquals(Expected_Title, Actual_Title, "title is not matching");
+		}catch(Throwable t)
+		{
+			System.out.println(t.getMessage());
+		}
+	}
+	
+	//method for closing browser
+	public static void closeBrowser(WebDriver driver)
+	{
+		driver.quit();
+	}
 }
--- src/test/java/driverFactory/DriverScript.java
+++ src/test/java/driverFactory/DriverScript.java
@@ -1,5 +1,60 @@
 package driverFactory;
 
-public class DriverScript {
+import org.apache.bcel.generic.ObjectType;
+import org.openqa.selenium.WebDriver;
 
-}
+import common.FunctionsLibrary;
+import utilities.ExcelFileUtil;
+
+public class DriverScript extends FunctionsLibrary {
+
+	String inputpath = "D:\\eclipse\\workspace\\ERP_Hybrid\\FileInput\\DataEngine.xlsx";
+	String outputpath = "D:\\eclipse\\workspace\\ERP_Hybrid\\FileOutput\\results.xlsx";
+	
+	public void startTest()throws Throwable
+	{
+		String Module_Status = "";
+		//call the exilfile util class method
+		ExcelFileUtil xl = new ExcelFileUtil(inputpath);
+		//iterate all rows in mastertestcase sheet
+		
+		for(int i=1;i<=xl.rowcount("MasterTestCases");i++)
+		{
+			if(xl.getcelldata("MasterTestCases", i, 2).equalsIgnoreCase("y"))
+			{
+			//store corresponding sheet into variable
+			String TCModule = xl.getcelldata("MasterTestCases", i, 1);
+			//iterate all rows in TCModule
+			for(int j=1;j<=xl.rowcount(TCModule);j++)
+			{
+				//call all cells
+				String Description = xl.getcelldata(TCModule, j, 0);
+				String ObjectType = xl.getcelldata(TCModule, j, 1);
+				String LocatorType = xl.getcelldata(TCModule, j, 2);
+				String LocatorValue = xl.getcelldata(TCModule, j, 3);
+				String  TestData = xl.getcelldata(TCModule, j, 4);
+				try {
+					if(ObjectType.equalsIgnoreCase("startBrowser"))
+					{
+						driver = FunctionsLibrary.StartBrowser();
+					}
+					else if(ObjectType.equalsIgnoreCase("openUrl"))
+					{
+						FunctionsLibrary.openurl(driver);
+					}
+				}
+			catch(Exception e)
+					{
+				System.out.println(e.getMessage());
+					}
+			}
+			}
+			else
+		{
+			//write a block which are flag to N
+				xl.setcelldata("", i, 3, "Blocked",outputpath);
+		}
+		
+	}
+	}
+		
